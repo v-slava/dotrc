57200ed9baafb74099b9f5a6d8e27e1f65bcd710
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2015-04-18 14:46:48 +0300

    Update bluetooth connect script

--- other_files/bluetooth_toggle.sh
+++ other_files/bluetooth_toggle.sh
@@ -4,11 +4,26 @@
 # If was connected    -> disconnecting
 # If was disconnected -> connecting
 
-if ~/os_settings/other_files/bluetooth_connected.sh ; then
-	echo -e "\nDisconnecting ...\n"
+set -e
+
+BLUETOOTH_CONNECTED=~/os_settings/other_files/bluetooth_connected.sh
+
+if $BLUETOOTH_CONNECTED ; then
 	echo "disconnect $BT_MAC" | bluetoothctl
+	sleep 1
+	while ! $BLUETOOTH_CONNECTED ; do
+		sleep 1
+	done
+	zenity --info --title "bluetooth headset" --text "Disconnected."
 else
-	echo -e "\nConnecting ...\n"
 	echo "connect $BT_MAC" | bluetoothctl
+	sleep 1
+	BT_SINK=$(pacmd list-sinks | grep -o "bluez_sink\.$(echo $BT_MAC | sed 's/:/\./g')")
+	while [ -z "$BT_SINK" ]; do
+		sleep 1
+		BT_SINK=$(pacmd list-sinks | grep -o "bluez_sink\.$(echo $BT_MAC | sed 's/:/\./g')")
+	done
+	pacmd set-default-sink $BT_SINK
+	zenity --info --title "Bluetooth headset" --text "Connected to $(hcitool name $BT_MAC) (MAC = $BT_MAC)."
 fi
 
