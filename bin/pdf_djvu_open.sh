1a5dc38aca165513985c97070655aa5c1f52e2e1
Viacheslav Volkov <v.volkov@samsung.com>
2015-07-09 13:07:44 +0300

    Update configs

--- bin/pdf_djvu_open.sh
+++ bin/pdf_djvu_open.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PASSWORD_FILE=/media/files/passwords/pdf_djvu_password
+PASSWORD_FILE_PREFIX=/media/files/passwords/pdf_djvu_password
 
 if [ $# -ne 1 ]; then
 	echo "Usage: $(basename $0) FILE.{pdf|djvu}" 1>&2
@@ -9,9 +9,14 @@ fi
 FILE="$1"
 
 CMD="zathura --fork"
-if [ -f $PASSWORD_FILE ]; then
-	PASSWORD=$(head -n 1 $PASSWORD_FILE)
-	CMD="$CMD -w '$PASSWORD'"
+if [ "$(echo "$FILE" | cut -c -5)" = "pass_" ]; then
+	PASSWORD_FILE_SUFFIX=$(echo "$FILE" | cut -c 6- | cut -d'_' -f1)
+	PASSWORD_FILE=$(echo "${PASSWORD_FILE_PREFIX}_${PASSWORD_FILE_SUFFIX}")
+	echo "a = $PASSWORD_FILE"
+	if [ -f $PASSWORD_FILE ]; then
+		PASSWORD=$(head -n 1 $PASSWORD_FILE)
+		CMD="$CMD -w '$PASSWORD'"
+	fi
 fi
 CMD="$CMD '$FILE' 2>/dev/null 1>&2"
 echo $CMD | bash
