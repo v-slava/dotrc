b7c235a1c2735a08737b2a0f6d6b438c543fc56e
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2015-07-25 12:52:32 +0300

    Update configs

---
+++ other_files/download_cnn_youtube.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $(basename $0) YouTube_URL" 1>&2
+	exit 1
+fi
+URL="$1"
+
+youtube-dl --write-sub "$URL"
+LAST_NUMBER=$(ls cnn_*.mp4 | tail -n 1 | cut -d'.' -f1 | cut -c 5-)
+let CUR_NUMBER=$LAST_NUMBER+1
+rename "s/CNN Student News[^.]*\./cnn_$CUR_NUMBER\./" *
+
