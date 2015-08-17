0a644c54a9c2ec98c65015c1ef9a7a2ae6bf679b
Viacheslav Volkov <v.volkov@samsung.com>
2015-08-17 10:10:58 +0300

    Update configs

---
+++ other_files/vimb_url_clipboard.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+die() {
+    echo "$1" >&2
+    exit 1
+}
+
+# check if the script is run from vimb with socket support enabled
+if [ -z "$VIMB_SOCKET" ] || [ ! -S "$VIMB_SOCKET" ]; then
+    die 'This script must be run from vimb with socket support'
+fi
+
+URL_REQUESTED="$(clipboard.sh -o)"
+echo ":o $URL_REQUESTED<CR>" | socat - unix-connect:"$VIMB_SOCKET"
+
