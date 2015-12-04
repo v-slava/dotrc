e88789d2e807f2241451d5ad32265bc7b57ea9f1
Viacheslav Volkov <v.volkov@samsung.com>
2015-12-04 14:52:38 +0200

    Update configs

--- other_files/lock_screen.sh
+++ other_files/lock_screen.sh
@@ -3,7 +3,11 @@
 set -e
 
 setxkbmap us
-xmodmap -e 'remove Lock = Caps_Lock' -e 'keycode 9 = Control_L'
+# Turn off Caps_Lock if needed:
+if xset q | grep -q 'Caps Lock:   on' ; then
+	xdotool key Caps_Lock
+fi
+xmodmap -e 'remove Lock = Caps_Lock' -e 'keycode 66 = Control_L'
 i3lock -c 101010 --nofork
 setxkbmap us,ru
 ~/os_settings/other_files/keyboard_layout.sh
