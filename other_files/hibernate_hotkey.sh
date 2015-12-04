84dfda23aa29eb0593bdf4503a997db53e65bde6
Viacheslav Volkov <v.volkov@samsung.com>
2015-12-04 14:36:19 +0200

    Update configs

--- other_files/hibernate_hotkey.sh
+++ other_files/hibernate_hotkey.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-~/os_settings/other_files/lock_screen.sh
+~/os_settings/other_files/lock_screen.sh &
 x-terminal-emulator -title "hibernate script" -e bash -c 'source ~/.bashrc && exec ~/os_settings/other_files/hibernate.sh'
 
--- other_files/lock_screen.sh
+++ other_files/lock_screen.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+set -e
+
 setxkbmap us
-i3lock -c 101010
+xmodmap -e 'remove Lock = Caps_Lock' -e 'keycode 9 = Control_L'
+i3lock -c 101010 --nofork
 setxkbmap us,ru
-
 ~/os_settings/other_files/keyboard_layout.sh
-
-# sleep 2
-# xset dpms force off
 
