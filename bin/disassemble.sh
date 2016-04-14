80287cc0d1603de3eb81dc3b0625c858c6a88260
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-04-14 09:17:48 +0200

    Update configs

--- bin/disassemble.sh
+++ bin/disassemble.sh
@@ -4,20 +4,31 @@
 # System uses EL (little endian), but objdump produces EB.
 # ARM architecture reference manual also uses EB.
 
-if [ $# -ne 3 ]; then
-	echo "Usage $(basename $0) {-a|-t} {-EB|-EL} {FILE|INSTRUCTION}" 1>&2
+usage()
+{
+	echo "Usage $(basename $0) {-a|-t} {-EB|-EL} {FILE|INSTRUCTION_EL}" 1>&2
 	exit 1
+}
+
+if [ $# -ne 3 ]; then
+    usage
 fi
 
 if [ "$1" = "-t" ]; then
-	THUMB=" -Mforce-thumb "
+	THUMB="-Mforce-thumb"
+else
+    if [ "$1" != "-a" ]; then
+        usage
+    fi
 fi
+if [ "$2" != "-EB" ] && [ "$2" != "-EL" ]; then
+    usage
+fi
+ENDIANESS="$2"
 
 TMP_FILE=/tmp/disassemble_sh_tmp
 set -e
-if [ -f "$TMP_FILE" ]; then
-	rm "$TMP_FILE"
-fi
+rm -f "$TMP_FILE"
 
 if [ -f "$3" ]; then
 	IN_FILE="$3"
@@ -27,5 +38,5 @@ else
 fi
 
 xxd -r -p "$IN_FILE" "$TMP_FILE"
-${CROSS_COMPILE}objdump -D -b binary "$2" -marm "$THUMB" "$TMP_FILE" | tail -n +8
+${CROSS_COMPILE}objdump -D -b binary $ENDIANESS -marm $THUMB "$TMP_FILE" | tail -n +8
 
