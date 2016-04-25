d03105989793a64b3ed155447184a4bdf54667eb
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-04-25 16:26:05 +0200

    Update configs

--- bin/build_server_mount.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sshfs -o allow_root,nonempty bld@$IP_BUILD_SERVER:/ /media/build_server
-
--- bin/exchange_mount.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sshfs -o allow_root,nonempty v.volkov@seclab.surc.kiev.ua:/home/exchange /media/exchange
-# sudo mount -t cifs -o user=v.volkov,domain=SURC //seclab.surc.kiev.ua/Exchange /media/exchange
-
