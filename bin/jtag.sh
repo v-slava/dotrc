ba7558a3d4274ae6a2cc8b2176b8e868a62fba78
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-04-26 11:11:48 +0200

    Update configs

--- bin/jtag.sh
+++ bin/jtag.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-~/other/programs/JLink_Linux_V512a_x86_64/JLinkGDBServer -device ATSAMV71Q21 -endian little -if SWD -speed auto
+usage()
+{
+	echo "Usage: $(basename $0) {big|small}" 1>&2
+	exit 1
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+if [ "$1" = "big" ]; then
+	DEVICE="ATSAM4E16E"
+	INTERFACE="JTAG"
+else
+	if [ "$1" = "small" ] || [ "$1" = "little" ]; then
+		DEVICE="ATSAMV71Q21"
+		INTERFACE="SWD"
+	else
+		usage
+	fi
+fi
+
+set -x
+~/other/programs/JLink_Linux_V512a_x86_64/JLinkGDBServer -device "$DEVICE" -endian little -if "$INTERFACE" -speed auto
 
