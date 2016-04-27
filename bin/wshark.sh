e88391e8ec9ef77a13db801e8d03f8dfaa9710a7
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-04-27 16:37:31 +0200

    Update configs

--- bin/wshark.sh
+++ bin/wshark.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-tcpdump -i "$@" -w - -U | wireshark -k -i -
+usage()
+{
+	echo -e "Usage: $(basename $0) INTERFACE
+
+List of available interfaces:\n" 1>&2
+	tcpdump -D 1>&2
+	exit 1
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+tcpdump -i "$1" -w - -U | wireshark -k -i -
 
