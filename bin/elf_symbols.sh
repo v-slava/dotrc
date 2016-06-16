2ed05229db6ecd1b619a89a21042b626314cfc6e
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-06-16 15:41:26 +0200

    Update configs

---
+++ bin/elf_symbols.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+USAGE="Usage: $(basename $0) ELF_FILE"
+
+usage()
+{
+	echo -e "$USAGE" 1>&2
+	exit 1
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+
+ELF_FILE="$1"
+
+${CROSS_COMPILE}nm --defined-only "$ELF_FILE" | sed -e '/ \$[dt]$/d' | cut -d' ' -f3 | ${CROSS_COMPILE}c++filt | sort
+
