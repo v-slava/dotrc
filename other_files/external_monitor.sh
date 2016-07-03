c9ed8865aac00c4a8306089f2001acee0739bf6a
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2016-07-03 12:40:34 +0200

    Update configs

--- other_files/external_monitor.sh
+++ other_files/external_monitor.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo -u volkov DISPLAY=:0 zenity --info --title "$(basename $0)" --text "$0 has been called due to /etc/udev/rules.d/95-external-monitor.rules"
+id=`id -u`
+safeuser="volkov"
+
+if [ $id = "0" ]; then
+	sudo -u $safeuser $0
+	exit
+fi
+
+DISPLAY=:0 zenity --info --title "$(basename $0)" --text "$0 has been called due to /etc/udev/rules.d/95-external-monitor.rules"
 
