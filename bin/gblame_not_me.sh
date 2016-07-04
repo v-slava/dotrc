c60fb9eaccf1945db81c185c7cfd3b8bf39bf842
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-07-04 08:56:47 +0200

    Update configs

--- bin/gblame_not_me.sh
+++ bin/gblame_not_me.sh
@@ -3,7 +3,12 @@
 set -e
 
 MY_NAME="$(git config user.name)"
+REGEX="^[^ ]\+ (${MY_NAME}"
+
 find -type f | while read file ; do
-	git blame "$file" | grep -v "^[^ ]\+ (${MY_NAME}"
+	if git blame "$file" | grep -v -q "$REGEX" ; then
+		echo "File: \"$file\""
+		git blame "$file" | grep -v "$REGEX"
+	fi
 done
 
