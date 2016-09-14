8285af06b2687defa10495e3b5b3c0839476e41c
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2016-09-14 07:40:28 +0200

    Update configs

--- other_files/hibernate_hotkey.sh
+++ other_files/hibernate_hotkey.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-~/os_settings/other_files/lock_screen.sh &
+# ~/os_settings/other_files/lock_screen.sh &
 x-terminal-emulator -title "hibernate script" -e bash -c \
 'source ~/.bashrc && ~/os_settings/other_files/update_system.sh && exec sudo systemctl hibernate'
 
