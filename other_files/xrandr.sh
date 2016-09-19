daebf70bea3a158d8169c07042d094944d793d61
Viacheslav Volkov <Viacheslav.Volkov@malighting.de>
2016-09-19 08:30:42 +0200

    Update configs

--- other_files/xrandr.sh
+++ other_files/xrandr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# master:
 if [ "$1" = "xinitrc" ]; then
 	xrandr --output LVDS1 --mode 1366x768
 fi
+
 if xrandr | grep -q 'HDMI1 connected' ; then
 	xrandr --output HDMI1 --mode 1680x1050 --right-of LVDS1
 else
