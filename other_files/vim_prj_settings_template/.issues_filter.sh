516012140cb7ccbb2a7cddde285778ec0c485dab
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2016-11-08 14:15:41 +0100

    Update configs

--- other_files/vim_prj_settings_template/.issues_filter.sh
+++ other_files/vim_prj_settings_template/.issues_filter.sh
@@ -5,5 +5,11 @@
 
 # /home/user/workspace/my_prj/main.c:49:3: warning: Value stored to 'tmp' is never read
 
-grep -v "/home/user/workspace/my_prj/main\.c:49:3: warning: Value stored to 'tmp' is never read" | grep -v ...
+# grep -v "/home/user/workspace/my_prj/main\.c:49:3: warning: Value stored to 'tmp' is never read" | grep -v ...
+
+END='[^.]\+\.\(c\|cpp\|h\|hpp\):[0-9]\+:[0-9]\+: warning'
+
+grep    -e 'error' \
+        -e "folder_1/$END"                                                     \
+        -e "folder_2/subfolder_1/prefix_$END"                                  \
 
