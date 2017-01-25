d3b4fb436fe6a2d941e50f8873111ede385f71ee
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2017-01-25 08:30:28 +0100

    Update configs

--- other_files/git_show_HEAD.sh
+++ other_files/git_show_HEAD.sh
@@ -3,39 +3,57 @@
 usage()
 {
 	echo -e "$(basename $0) [-n] [-r] [NUM_COMMITS_BACK_FROM_HEAD]
-Options mapping:
--n => --name-only
--r => --relative" 1>&2
-	exit 1
+-h                 => show this help
+-n [ARG]           => git show --name-only HEAD[~ARG]
+-r [ARG]           => git show --relative HEAD[~ARG]
+-p [<path>...]     => git diff HEAD~1 HEAD -- [<path>...]
+-P ARG [<path>...] => git diff HEAD~{ARG-1} HEAD~ARG -- [<path>...]
+
+Note: ARG must be specified without '~'. For example:
+$(basename $0) -n 1
+" 1>&2
+	exit $1
 }
 
-CMD="git show"
-NUM_OPTIONS_FOUND=0
+set_cmd()
+{
+	if [ -n "$CMD" ]; then
+		usage 1
+	fi
+	CMD="$1"
+}
 
-while getopts "nr" arg ; do
+SINGLE_ARG=0
+while getopts "hnrpP:" arg ; do
 	case "$arg" in
+		h)
+			usage 0
+			;;
 		n)
-			CMD="$CMD --name-only"
-			NUM_OPTIONS_FOUND=$((NUM_OPTIONS_FOUND + 1))
+			SINGLE_ARG=1
+			set_cmd "git show --name-only HEAD"
 			;;
 		r)
-			CMD="$CMD --relative"
-			NUM_OPTIONS_FOUND=$((NUM_OPTIONS_FOUND + 1))
+			SINGLE_ARG=1
+			set_cmd "git show --relative HEAD"
+			;;
+		p)
+			set_cmd "git diff HEAD~1 HEAD --"
+			;;
+		P)
+			set_cmd "git diff HEAD~$((OPTARG - 1)) HEAD~$((OPTARG)) --"
 			;;
 	esac
 done
+shift $(($OPTIND - 1))
 
-while [ $NUM_OPTIONS_FOUND -gt 0 ] ; do
-	NUM_OPTIONS_FOUND=$((NUM_OPTIONS_FOUND - 1))
-	shift
-done
-
-if [ $# -gt 1 ]; then
-	usage
+if [ $SINGLE_ARG -eq 1 ]; then
+	if [ $# -gt 1 ]; then
+		usage 2
+	fi
+	if [ -n "$1" ]; then
+		CMD="${CMD}~${1}"
+		shift
+	fi
 fi
-
-CMD="$CMD HEAD"
-if [ -n "$1" ]; then
-	CMD="${CMD}~${1}"
-fi
-$CMD
+$CMD "$@"
