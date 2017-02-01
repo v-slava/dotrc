5bc3063c8a70d7c1c0e1809f60fa7e1186ca5f8e
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2017-02-01 11:25:40 +0100

    Update configs

--- other_files/git_show_HEAD.sh
+++ other_files/git_show_HEAD.sh
@@ -2,15 +2,11 @@
 
 usage()
 {
-	echo -e "$(basename $0) [-n] [-r] [NUM_COMMITS_BACK_FROM_HEAD]
--h                 => show this help
--n [ARG]           => git show --name-only HEAD[~ARG]
--r [ARG]           => git show --relative HEAD[~ARG]
--p [<path>...]     => git diff HEAD~1 HEAD -- [<path>...]
--P ARG [<path>...] => git diff HEAD~{ARG-1} HEAD~ARG -- [<path>...]
-
-Note: ARG must be specified without '~'. For example:
-$(basename $0) -n 1
+	echo -e "\
+$(basename $0) -h                 => show this help
+$(basename $0) [-n] [-r] [ARG]    => git show [--name-only] [--relative] HEAD[~ARG]
+$(basename $0) -p [<path>...]     => git diff HEAD~1 HEAD -- [<path>...]
+$(basename $0) -P ARG [<path>...] => git diff HEAD~{ARG-1} HEAD~ARG -- [<path>...]
 " 1>&2
 	exit $1
 }
@@ -23,38 +19,41 @@ set_cmd()
 	CMD="$1"
 }
 
-SINGLE_ARG=0
+add_cmd()
+{
+	CMD="$CMD $1"
+}
+
+CMD="git show"
+EXPECT_COMMIT_NUMBER=1
 while getopts "hnrpP:" arg ; do
 	case "$arg" in
 		h)
 			usage 0
 			;;
 		n)
-			SINGLE_ARG=1
-			set_cmd "git show --name-only HEAD"
+			add_cmd "--name-only"
 			;;
 		r)
-			SINGLE_ARG=1
-			set_cmd "git show --relative HEAD"
+			add_cmd "--relative"
 			;;
 		p)
 			set_cmd "git diff HEAD~1 HEAD --"
+			EXPECT_COMMIT_NUMBER=0
 			;;
 		P)
 			set_cmd "git diff HEAD~$((OPTARG - 1)) HEAD~$((OPTARG)) --"
+			EXPECT_COMMIT_NUMBER=0
 			;;
 	esac
 done
 shift $(($OPTIND - 1))
 
-if [ -z "$CMD" ]; then
-	CMD="git show HEAD"
-fi
-
-if [ $SINGLE_ARG -eq 1 ]; then
+if [ $EXPECT_COMMIT_NUMBER -eq 1 ]; then
 	if [ $# -gt 1 ]; then
 		usage 2
 	fi
+	CMD="$CMD HEAD"
 	if [ -n "$1" ]; then
 		CMD="${CMD}~${1}"
 		shift
