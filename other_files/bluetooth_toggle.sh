01d0cfb653bc742cca91eb6c7d95f18252e23f1d
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2017-05-13 22:25:12 +0200

    Update configs.

--- other_files/bluetooth_toggle.sh
+++ other_files/bluetooth_toggle.sh
@@ -10,6 +10,10 @@ BLUETOOTH_CONNECTED=~/os_settings/other_files/bluetooth_connected.sh
 
 if $BLUETOOTH_CONNECTED ; then # connected => need to disconnect
 
+	set -e
+	SPEAKERS_SINK="$(~/os_settings/other_files/get_sink_name.sh alsa_output)"
+	pactl -- set-sink-mute "$SPEAKERS_SINK" 1
+	set +e
 	cat << EOF | expect 2>&1
 set timeout 10
 spawn bluetoothctl
@@ -33,7 +37,9 @@ EOF
 		echo "Failed to disconnect from bluetooth headset $BT_MAC." 1>&2
 		exit 1
 	fi
-	~/os_settings/other_files/set_volume.sh 20%
+	set -e
+	~/os_settings/other_files/set_volume.sh 30%
+	pactl -- set-sink-mute "$SPEAKERS_SINK" 0
 	echo "Disconnected from bluetooth headset $BT_MAC."
 
 else # disconnected => need to connect
@@ -72,18 +78,22 @@ EOF
 			echo -e "Failed to connect to bluetooth headset $BT_MAC.\n" 1>&2
 		fi
 	done # while [ $bluetoothctl_RET -ne 1 ]
+	set -e
+	echo "Attempting to get full sink name for pattern bluez_sink.* ..."
+	BT_SINK="$(~/os_settings/other_files/get_sink_name.sh bluez_sink)"
+	pactl -- set-sink-mute "$BT_SINK" 1
 	# Set bluetooth profile
 	CARD_NAME=$(~/os_settings/other_files/get_card_name.sh bluez_card)
+	set +e
 	RET=1
 	while [ $RET -ne 0 ]; do
 		pactl set-card-profile "$CARD_NAME" a2dp_sink 2>/dev/null
 		RET=$?
 	done
 	set -e
-	echo "Attempting to get full sink name for pattern bluez_sink.* ..."
-	BT_SINK="$(~/os_settings/other_files/get_sink_name.sh bluez_sink)"
 	pacmd set-default-sink "$BT_SINK"
 	~/os_settings/other_files/set_volume.sh 20%
+	pactl -- set-sink-mute "$BT_SINK" 0
 	# Move application producing sound to bluetooth headset:
 	SINK_INPUT_INDICES=$(pacmd list-sink-inputs | grep 'index: ' | cut -d':' -f2 | cut -d' ' -f2)
 	for sink_input_index in $SINK_INPUT_INDICES ; do
@@ -91,4 +101,3 @@ EOF
 	done
 	echo "Connected to bluetooth headset $(hcitool name $BT_MAC) (MAC = $BT_MAC)."
 fi
-
