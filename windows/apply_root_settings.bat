3bad33f0949dd8d46d5757418a4aac1ba1eacd5b
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2017-10-27 15:22:10 +0200

    Update configs

--- windows/apply_root_settings.bat
+++ windows/apply_root_settings.bat
@@ -1,6 +1,11 @@
 cd %~dp0
 call settings.bat || goto error
-mklink %homepath%\os_settings %DOTRC% || goto error
+if not exist %homepath%\os_settings (
+    mklink %homepath%\os_settings %DOTRC% || goto error
+)
+if not exist %homepath%\.spacemacs (
+    mklink %homepath%\.spacemacs %homepath%\os_settings\other_files\dot_spacemacs.el || goto error
+)
 exit
 :error
 set /p DUMMY=Hit ENTER to exit...
