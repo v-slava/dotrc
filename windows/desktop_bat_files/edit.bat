b1df69200edd93810d0e80123eee9598f1067658
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2017-10-30 11:57:26 +0100

    Update configs.

--- windows/desktop_bat_files/edit.bat
+++ windows/desktop_bat_files/edit.bat
@@ -1,2 +1,6 @@
-call %~dp0\..\settings.bat
-%GIT_BASH% -c e
+call %~dp0\..\settings.bat || goto error
+%GIT_BASH% -c e || goto error
+goto end
+:error
+set /p DUMMY=Hit ENTER to exit...
+:end
--- windows/desktop_bat_files/git_bash.bat
+++ windows/desktop_bat_files/git_bash.bat
@@ -1,3 +1,7 @@
-call %~dp0\..\settings.bat
-:: cd %SOME_REPO_ROOT%
-%GIT_BASH%
+call %~dp0\..\settings.bat || goto error
+:: cd %SOME_REPO_ROOT% || goto error
+%GIT_BASH% || goto error
+goto end
+:error
+set /p DUMMY=Hit ENTER to exit...
+:end
