589426b7b49e8833b35e99388ba737ec04e1e748
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2018-03-17 11:51:42 +0100

    Update configs.

--- other_files/update_system.sh
+++ other_files/update_system.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LOCK_DIR=/tmp/system_update_lock_dir
-if ! mkdir "$LOCK_DIR" ; then
-	echo -e "Error: lock directory exists
-(system update has already been started in parallel process)."
-	exit 1
-fi
+# LOCK_DIR=/tmp/system_update_lock_dir
+# if ! mkdir "$LOCK_DIR" ; then
+# 	echo -e "Error: lock directory exists
+# (system update has already been started in parallel process)."
+# 	exit 1
+# fi
 set -x
 # sudo update-flashplugin-nonfree --install
 # sudo update-pepperflashplugin-nonfree --install
