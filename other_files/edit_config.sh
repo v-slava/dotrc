b469935c6d2eed6d52e96f5c32c09859e2d675c8
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2018-07-03 17:05:50 +0200

    Update configs

---
+++ other_files/edit_config.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+USAGE="Usage: $(basename $0) {dotrc|dotrc_s} path/to/file\n\
+Where path/to/file is relative to home folder."
+
+set -e
+
+usage()
+{
+    echo -e "$USAGE" 1>&2
+    exit 1
+}
+
+if [ $# -ne 2 ]; then
+    usage
+fi
+
+FILE="$2"
+
+DOTRC_PATH=~/os_settings/home_settings
+DOTRC_S_PATH=~/workspace/dotrc_s/home_settings
+DOTRC_FILE="$DOTRC_PATH/$FILE"
+DOTRC_S_FILE="$DOTRC_S_PATH/$FILE"
+if [ ! -f "$DOTRC_FILE" ]; then
+    echo "File not found: $DOTRC_FILE" 1>&2
+    exit 2
+fi
+
+case "$1" in
+    "dotrc")
+        EDIT_FILE="$DOTRC_FILE"
+        ;;
+    "dotrc_s")
+        EDIT_FILE="$DOTRC_S_FILE"
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+EDIT_DIR=$(dirname "$EDIT_FILE")
+if [ ! -d "$EDIT_DIR" ]; then
+    mkdir -p "$EDIT_DIR"
+fi
+
+e --wait "$EDIT_FILE"
+cp "$DOTRC_FILE" ~/$FILE
+if [ -f "$DOTRC_FILE" ]; then
+    cat "$DOTRC_S_FILE" >> ~/$FILE
+fi
