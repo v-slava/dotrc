ca09344afdaf5092575bea5b323d69f40b0be22a
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2018-08-17 16:51:06 +0200

    Update configs

--- other_files/hibernate_hotkey.sh
+++ other_files/hibernate_hotkey.sh
@@ -3,10 +3,10 @@
 set -e
 
 if $DOTRC/other_files/virtual_box.sh ; then
-	sudo poweroff
+    sudo poweroff
 else
-	$DOTRC/other_files/lock_screen.sh &
-	x-terminal-emulator -title "hibernate script" -e bash -c \
+    $DOTRC/other_files/lock_screen.sh &
+    x-terminal-emulator -title "hibernate script" -e bash -c \
 'source ~/.bashrc && $DOTRC/other_files/update_system.sh && exec sudo poweroff'
     # sudo systemctl hibernate
 fi
