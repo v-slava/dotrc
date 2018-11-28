b03839f2e83f50ac53ff11b856e2b43d33987207
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2018-11-28 11:29:59 +0100

    Update configs

--- other_files/objdump.sh
+++ other_files/objdump.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # This is an objdump wrapper script. It determines target file type and runs
-# proper objdump executable (either objdump or ${CROSS_COMPILE}objdump)
+# proper objdump executable (either objdump or ${MY_CROSS_COMPILE}objdump)
 
 # last command line argument:
 FILE="${@: -1}"
 
 if file "$FILE" | grep -q "ARM" ; then
-	OBJDUMP=${CROSS_COMPILE}objdump
+	OBJDUMP=${MY_CROSS_COMPILE}objdump
 else
 	OBJDUMP=objdump
 fi
