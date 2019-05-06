7218b64c80ef0683c3feacd0b1bba6784f43f093
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-05-06 20:36:31 +0200

    Update configs

---
+++ other_files/wait_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INTERVAL=30
+
+$DOTRC/other_files/lock_screen.sh
+
+while pgrep "$1" ; do
+    sleep 30
+done
+echo "Shutting down after $INTERVAL seconds..."
+sync
+sleep $INTERVAL
+sync
+sudo poweroff
