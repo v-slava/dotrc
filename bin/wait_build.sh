e44a1343f80f74453b183abb1b3dfecee1064d05
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-06-09 23:59:53 +0200

    Update configs

--- bin/wait_build.sh
+++ bin/wait_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 INTERVAL=30
+LOG=/tmp/wait_build_log
 
+echo "$(date): wait_build.sh started..." | tee $LOG
+
+echo "lock_screen.sh PID:"
 $DOTRC/other_files/lock_screen.sh &
 
 if ! declare -f -F build_running ; then
@@ -14,9 +18,9 @@ fi
 while build_running ; do
     sleep $INTERVAL
 done
-echo "$(date): shutting down after $INTERVAL seconds..." | tee /tmp/wait_build_log
+echo "$(date): shutting down after $INTERVAL seconds..." | tee -a $LOG
 sync
 sleep $INTERVAL
+echo "$(date): shutting down NOW!" | tee -a $LOG
 sync
 sudo poweroff
-echo "powered off!"
