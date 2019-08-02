315b91b3d81b5042053ef09c51ad564f1391674b
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-08-02 19:52:59 +0200

    Update configs

--- other_files/mail_client.sh
+++ other_files/mail_client.sh
@@ -2,5 +2,8 @@
 
 while [ true ]; do
     thunderbird
+    if pidof thunderbird 1>/dev/null ; then
+        break
+    fi
     zenity --error --text '<span foreground="red" font="32">Do not close thunderbird window!</span>\n\n<i>It will be restarted automatically for now</i>'
 done
