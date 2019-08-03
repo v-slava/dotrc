be680510b3d19e23650fc73cf36e021019a3a05e
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-08-03 13:41:33 +0200

    Update configs

--- other_files/dmenu.sh
+++ other_files/dmenu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-source ~/.bashrc
+# source ~/.bashrc
 # i3-dmenu-desktop --dmenu='dmenu -i -fn Inconsolata LGC-16:monospace' --entry-type=command
-dmenu_run -fn 'Inconsolata LGC-16:monospace'
+# exec dmenu_run -fn 'Inconsolata LGC-16:monospace'
+
+# The following implementation avoids excessive bash process:
+CMD="$(dmenu_path | dmenu -fn 'Inconsolata LGC-16:monospace' "$@")"
+if [ $? -eq 0 ]; then
+    exec "$CMD"
+fi
