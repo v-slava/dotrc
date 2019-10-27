2e9be1fcc520a7c670115183acbc1677ca31e7a7
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-10-27 10:51:49 +0100

    update

--- other_files/audio_player_play_files.sh
+++ other_files/audio_player_play_files.sh
@@ -12,6 +12,23 @@ if [ -n "$PID" ]; then
     kill $PID
 fi
 
+if [[ $PWD == /media/files/music/new* ]]; then
+    for file in "$@" ; do
+        if ! id3v2 -l "$file" | grep -q ': No ID3 tag$' ; then
+#             cat << EOF 1>&2
+# Error: ID3 tags found for $file. Delete tags by running:
+#
+# id3v2 -D "$(realpath "$file")"
+#
+# and try playing it again
+# EOF
+#             exit 1
+            echo "Removing ID3 tags for $file ..."
+            id3v2 -D "$file"
+        fi
+    done
+fi
+
 cat << EOF | xmms2 > /dev/null
 stop
 remove *
