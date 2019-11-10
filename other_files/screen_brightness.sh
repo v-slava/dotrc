88eab84c956b97f82b0182634ff248b6cb97b720
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-11-10 06:33:48 +0100

    update

---
+++ other_files/screen_brightness.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Usage: $(basename $0) {--up|--down}" 1>&2
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+case "$1" in
+    "--up") OP="-inc" ;;
+    "--down") OP="-dec" ;;
+    *) usage
+esac
+
+CUR_VAL=$(xbacklight -get | cut -d'.' -f1)
+AMOUNT=$((CUR_VAL / 10))
+if [ "$AMOUNT" -eq 0 ]; then
+    AMOUNT=1
+fi
+xbacklight $OP $AMOUNT
