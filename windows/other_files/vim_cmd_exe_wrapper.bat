7c694a2c633bf0ea3877d99591ddfbaf0493b157
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-11-14 09:42:00 +0100

    update

--- windows/other_files/vim_cmd_exe_wrapper.bat
+++ windows/other_files/vim_cmd_exe_wrapper.bat
@@ -1,6 +1,8 @@
 @echo off
+:: if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 echo Command: %* Command output:
-%*
+%* 2>&1 || goto continue
+:continue
 echo.
 if %errorlevel% equ 0 (
     echo Command succeeded ^(exit code = 0^)
