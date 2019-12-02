ce0c82a3e126e4c367310bd68511dbe6caf7e34a
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2019-12-02 09:24:34 +0100

    Update configs

--- other_files/config_file.sh
+++ other_files/config_file.sh
@@ -95,23 +95,28 @@ config_generate_home()
     set -e
     case "$FILE" in
         ".mbsyncrc")
-            config_preprocess
-            chmod 600 "$DEST"
-            if [ "$(id -u)" != "0" ]; then
-                mkdir -p $HOME/mail/slava65536@gmail.com
-                mkdir -p $HOME/mail/viacheslav.volkov.1@gmail.com
-                # Add short symlinks for sidebar in neomutt:
-                if [ ! -L $HOME/mail/s ]; then
-                    ln -s $HOME/mail/slava65536@gmail.com $HOME/mail/s
-                fi
-                if [ ! -L $HOME/mail/v ]; then
-                    ln -s $HOME/mail/viacheslav.volkov.1@gmail.com $HOME/mail/v
+            if ! $DOTRC/other_files/virtual_box.sh ; then
+                config_preprocess
+                chmod 600 "$DEST"
+                if [ "$(id -u)" != "0" ]; then
+                    mkdir -p $HOME/mail/slava65536@gmail.com
+                    mkdir -p $HOME/mail/viacheslav.volkov.1@gmail.com
+                    # Add short symlinks for sidebar in neomutt:
+                    if [ ! -L $HOME/mail/s ]; then
+                        ln -s $HOME/mail/slava65536@gmail.com $HOME/mail/s
+                    fi
+                    if [ ! -L $HOME/mail/v ]; then
+                        ln -s $HOME/mail/viacheslav.volkov.1@gmail.com \
+                            $HOME/mail/v
+                    fi
                 fi
             fi
             ;;
         ".msmtprc")
-            config_preprocess
-            chmod 600 "$DEST"
+            if ! $DOTRC/other_files/virtual_box.sh ; then
+                config_preprocess
+                chmod 600 "$DEST"
+            fi
             ;;
         ".spacemacs" | \
         ".bashrc")
