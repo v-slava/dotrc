fdd4ca01a1dfa8f9b3d7bc97ab1a60a787ad3e16
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2020-03-02 11:29:13 +0100

    update

--- other_files/handle_terminal_buffer.py
+++ other_files/handle_terminal_buffer.py
@@ -10,3 +10,14 @@
 # '^Changes to be commited:[ ]*$'
 # '^Changes not staged for commit:[ ]*$'
 # '^Untracked files:[ ]*$'
+
+import sys
+lines = [line.rstrip('\n') for line in sys.stdin.readlines()]
+
+with open('/tmp/stdin', 'w') as f:
+    for line in lines:
+        f.write(line + '\n')
+
+import subprocess
+subprocess.run(["zenity", "--info", "--title", str(sys.argv), "--text",
+    '{' + '\n'.join(lines) + '}'])
