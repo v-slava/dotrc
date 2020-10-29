e1d9d6d065ab09bf939171b66bb9258910a804d0
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2020-10-29 20:02:04 +0200

    switch to new bear version

--- other_files/build_or_install_scripts/bear/bear.sh
+++ other_files/build_or_install_scripts/bear/bear.sh
@@ -2,26 +2,17 @@
 
 set -e
 
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PATCH="$THIS_DIR/bear.patch"
 TMP="/media/files/workspace/tmp"
-# PROGRAMS="/media/files/programs"
-# BEAR_OUT="$PROGRAMS/bear"
 
 set -x
-# rm -rf "$BEAR_OUT"
-# mkdir -p "$BEAR_OUT"
 mkdir -p "$TMP"
 cd "$TMP"
 rm -rf Bear
 git clone https://github.com/rizsotto/Bear
 cd Bear
-git checkout 2.4.2
-git config user.email "viacheslav.volkov.1@gmail.com"
-git am < "$PATCH"
+git checkout 3.0.1
 mkdir out
 cd out
-# -DCMAKE_INSTALL_PREFIX="$BEAR_OUT"
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j5
 sudo make -j5 install
