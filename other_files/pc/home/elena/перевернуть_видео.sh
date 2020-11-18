1730620c3d54b8fdda86883d9a4bb130b582a6ce
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2020-11-18 22:17:13 +0200

    update

--- other_files/pc/home/elena/перевернуть_видео.sh
+++ other_files/pc/home/elena/перевернуть_видео.sh
@@ -6,8 +6,9 @@ OUT="$(echo "$IN" | cut -d'.' -f-1)_перевернутое.$(echo "$IN" | cut 
 echo "|$PWD|$IN|$OUT|" >> /tmp/rotate_video.sh
 OUT_CUT="$(echo "$OUT" | rev | cut -d'/' -f1 | rev)"
 zenity --info --title 'Подождите...' --text "Видео переворачивается..." &
+ZENITY_PID=$!
 ffmpeg -i "$IN" -c:a copy "$OUT"
-set +e
-killall zenity
-set -e
+if pidof zenity | grep -q $ZENITY_PID ; then
+    kill $ZENITY_PID
+fi
 zenity --info --title 'Успех' --text "Видео успешно перевернуто: создан новый файл $OUT_CUT"
