20b3559e084fdbaa2c985d95ddc62801e0182bdd
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2021-01-07 16:38:42 +0200

    update

---
+++ other_files/build_or_install_scripts/sispmctl/sispmctl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TMP="/media/files/workspace/tmp"
+
+set -ex
+sudo apt-get install libusb-dev
+mkdir -p "$TMP"
+cd "$TMP"
+rm -rf sispmctl
+git clone https://git.code.sf.net/p/sispmctl/git sispmctl
+cd sispmctl
+git tag
+git checkout release-4.8
+./autogen.sh
+./configure --enable-webless
+make
+sudo make install
+
+cat << EOF | sudo tee /etc/udev/rules.d/60-sispmctl.rules
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="04b4", \
+ATTRS{idProduct}=="fd15", MODE="660", GROUP="plugdev"
+EOF
+sudo udevadm control --reload
+# Now reconnect EG-PMS2 usb device.
+
+# scan for supported devices:
+# sispmctl -s
+
+# Turn on outlet 1:
+# sispmctl -q -o 1
+
+# Turn off outlet 4:
+# sispmctl -q -f 4
+
+# Turn on outlets 1, 2 and 4:
+# sispmctl -q -o 1 -o 2 -o 4
+
+# Turn off all outlets:
+# sispmctl -q -f all
+
+# Print status of outlet 2 (on|off):
+# sispmctl -q [-g|-m] 2
