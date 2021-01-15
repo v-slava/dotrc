5fc9da22facce06fd1efc2f6f714729aa8d6825a
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2021-01-15 12:57:30 +0200

    update

--- bin/process_compile_commands.py
+++ bin/process_compile_commands.py
@@ -5,19 +5,25 @@ import os, sys, json, argparse
 def parse_cmd_line_args():
     parser = argparse.ArgumentParser(description = 'Process compile_commands.json')
     def_file = 'compile_commands.json'
-    parser.add_argument('-i', '--input-file', default = def_file,
-            help = f'file to be processed (by default: {def_file})',
+    parser.add_argument('-i', '--input-file', default = [def_file], nargs = '+',
+            help = f'file(s) to be processed (by default: {def_file})',
             metavar = 'FILE')
+    parser.add_argument('-m', '--merge', action='store_true',
+                        help = 'merge input files and print result to stdout')
     parser.add_argument('-s', '--statistics', action='store_true',
                         help = 'show number of commands per compiler')
     parser.add_argument('-e', '--exclude-compilers', nargs = '+',
     help = 'compilers to exclude from compile_commands.json (changes the file)',
     metavar = 'COMPILER')
     args = parser.parse_args()
-    if not args.statistics and not args.exclude_compilers:
+    if not args.statistics and not args.exclude_compilers and not args.merge:
         parser.print_help()
         parser.exit(status = 0, message =
                 '\nNo arguments provided => nothing to do. Exiting...\n')
+    if args.merge and (args.statistics or args.exclude_compilers):
+        parser.print_help(sys.stderr)
+        parser.exit(status = 1, message =
+                '\nOption -m is incompatible with both -s and -e.\n')
     return args
 
 def get_compiler(cmd_json):
@@ -55,12 +61,13 @@ def show_statistics(input_file):
             compilers[compiler] = compilers[compiler] + 1
         else:
             compilers[compiler] = 1
-    for compiler in compilers:
+    sorted_compilers = {k: compilers[k] for k in sorted(compilers)}
+    for compiler in sorted_compilers:
         num = compilers[compiler]
         print(f'{compiler} : {num}')
     # compilers = set()
     # for c in cmd:
-    #     compiler = c['arguments'][0]
+    #     compiler = get_compiler(c)
     #     compilers.add(compiler)
     # print('\n'.join(compilers)
 
@@ -75,20 +82,41 @@ def filter_compilers(input_file, exclude_compilers):
     # num = len(cmd_out)
     # print(f'after filtering: {num} commands')
 
+def merge(input_files):
+    result = []
+    for input_file in input_files:
+        with open(input_file, 'r') as f:
+            cmd_in = json.loads(f.read())
+        result.extend(cmd_in)
+    # return result
+    # remove duplicates:
+    with_duplicates = [json.dumps(cmd) for cmd in result]
+    without_duplicates = list(set(with_duplicates))
+    result_without_duplicates = [json.loads(cmd) for cmd in without_duplicates]
+    return result_without_duplicates
+
 def main():
     # exclude_compilers = [
     #       'gcc',
     #       os.path.expandvars('$DOTRC_S/bin/gcc'),
     # ]
     args = parse_cmd_line_args()
-    if args.exclude_compilers:
-        print('Statistics before filtering:\n')
-        show_statistics(args.input_file)
-        filter_compilers(args.input_file, args.exclude_compilers)
-        print('\nStatistics after filtering:\n')
-        show_statistics(args.input_file)
-    elif args.statistics:
-        show_statistics(args.input_file)
+    if args.merge:
+        result = merge(args.input_file)
+        print(json.dumps(result, indent = 2))
+        return
+    num_inputs = len(args.input_file)
+    for input_file in args.input_file:
+        if num_inputs > 1:
+            print(f'{input_file}:')
+        if args.exclude_compilers:
+            print('Statistics before filtering:\n')
+            show_statistics(input_file)
+            filter_compilers(input_file, args.exclude_compilers)
+            print('\nStatistics after filtering:\n')
+            show_statistics(input_file)
+        elif args.statistics:
+            show_statistics(input_file)
 
 if __name__ == '__main__':
     main()
