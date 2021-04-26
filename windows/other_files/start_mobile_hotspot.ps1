9ea3451ca53d1a86c90be2ea28541b6c231edc3b
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2021-04-26 18:13:10 +0300

    update

--- windows/other_files/start_mobile_hotspot.ps1
+++ windows/other_files/start_mobile_hotspot.ps1
@@ -1,3 +1,4 @@
+# sleep 15
 $a = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager, Windows.Networking.NetworkOperators, ContentType=WindowsRuntime]::CreateFromConnectionProfile(
          [Windows.Networking.Connectivity.NetworkInformation, Windows.Networking.Connectivity, ContentType=WindowsRuntime]::GetInternetConnectionProfile()
      )
