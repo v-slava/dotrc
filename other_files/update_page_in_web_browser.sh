b8ead92fe6298219f15b6e5185637018f1d3d854
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2021-07-18 17:53:16 +0300

    update

--- other_files/update_page_in_web_browser.sh
+++ other_files/update_page_in_web_browser.sh
@@ -5,7 +5,21 @@ BROWSER_MARK=browser
 
 set -e
 
-swaymsg "mark --add $PREV_MARK, [con_mark=\"$BROWSER_MARK\"] focus"
+sleep 0.01 # sometimes browser doesn't detect that html has been updated..
+
+# The following doesn't work in sway:
+# swaymsg "mark --add $PREV_MARK, [con_mark=\"$BROWSER_MARK\"] focus"
+swaymsg "mark --add $PREV_MARK"
+swaymsg "[con_mark=\"$BROWSER_MARK\"] focus"
+
 # xdotool key F5
-wtype -k F5
+
+# The following doesn't work for me:
+# wtype -k f5
+# wtype -M ctrl
+# wtype l
+# wtype -m ctrl
+
+sudo ydotool key f5
+
 swaymsg "[con_mark=\"$PREV_MARK\"] focus"
