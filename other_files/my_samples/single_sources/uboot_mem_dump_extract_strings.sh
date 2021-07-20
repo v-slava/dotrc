5bf5b77f2b061b3651da1779b67a218bbfa54688
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2021-07-20 12:39:35 +0300

    update

--- other_files/my_samples/single_sources/uboot_mem_dump_extract_strings.sh
+++ other_files/my_samples/single_sources/uboot_mem_dump_extract_strings.sh
@@ -6,7 +6,7 @@
 # md.b 0xc0a3802c 3000
 
 # See also $DOTRC/usefull_info.txt on how to get address_of(__log_buf)
-# See also $DOTRC/other_files/bin_dump_extract_strings.py
+# See also $DOTRC/other_files/my_samples/single_sources/bin_dump_extract_strings.py
 
 address=0
 cut -d: -f2- | cut -d' ' -f -20 | while read line ; do
